597d209f7aba6ebe06b42185359f50d2c92ad025
thisisAhmedSalah <as5652478@gmail.com>
2023-10-07 20:55:58 -0700

    up

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Prints the alphabet.
+ * main - Prints the alphabet without q and e.
  *
  * Return: Always 0 (Success)
  */
@@ -16,5 +16,6 @@ int main(void)
 			putchar(i);
 		}
 	}
+	putchar("\n");
 	return 0;
-}
+}}
