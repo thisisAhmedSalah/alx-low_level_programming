a3cb3473d8ecb564dd2e8eee7b774c19ed06fcc3
thisisAhmedSalah <as5652478@gmail.com>
2023-10-11 18:17:06 -0700

    up

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,7 +11,7 @@
     char letter;
     for ( i = 0; i < 10; i++)
     {
-        for ( letter = 'a'; letter < 'z'; letter++)
+        for ( letter = 'a'; letter <= 'z'; letter++)
         {
             _putchar(letter);
         }
