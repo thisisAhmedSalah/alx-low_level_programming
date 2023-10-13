108e8999e0994b096748183bc9a7505ce469baa2
thisisAhmedSalah <as5652478@gmail.com>
2023-10-13 13:01:15 -0700

    up

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,7 +10,7 @@ void print_numbers(void)
 
     for (i = 48; i <= 57; i++)
     {
-        if (i == 2 || i == 4)
+        if (i == 50 || i == 52)
             continue;
         _putchar(i);
     }
