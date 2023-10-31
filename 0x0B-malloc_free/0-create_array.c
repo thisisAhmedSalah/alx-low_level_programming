bd5bd845409bc98cefb74fdcc502e70529442746
thisisAhmedSalah <as5652478@gmail.com>
2023-10-31 08:33:07 -0700

    up

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,21 +13,16 @@ char *create_array(unsigned int size, char c)
     unsigned int i;
     char *str;
 
-    // Allocate memory for the character array
     str = malloc(sizeof(char) * size);
     if (str == NULL || size == 0)
     {
-        // Free the memory and return NULL if an error occurred
         free(str);
         return NULL;
     }
-
-    // Fill the character array with the given character
     for (i = 0; i < size; i++)
     {
         str[i] = c;
     }
-
-    // Return a pointer to the created character array
     return str;
 }
+
